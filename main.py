48894f5a291b77375ee160144b49c38ea4cc93f7
Richard Song <song-s20@mails.tsinghua.edu.cn>
2023-06-02 15:50:19 +0000

    model analysis done

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from models.CA import CA3
-import torch
-from torch import nn
-from matplotlib import pyplot as plt
+from models.PCA import PCA
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from utils import charas
+import matplotlib.pyplot as plt
 
-model = CA3(6, 0.5)
-model.optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-model.criterion = nn.MSELoss()
+import warnings
+warnings.filterwarnings('ignore')
 
-valid_loss = model.train_model()
-print(valid_loss)
+
+def model_inference_and_predict(model):
+    mon_list = pd.read_pickle('data/mon_list.pkl')
+    test_mons = mon_list.loc[mon_list >= model.test_period[0]]
+    inference_result = []
+    predict_result = []
+    T_bar = tqdm(test_mons.groupby(test_mons.apply(lambda x: x//10000)), colour='red', desc=f'{model.name} Inferencing & Predicting')
+    
+    for g in T_bar: # rolling train
+        T_bar.set_postfix({'Year': g[0]})
+        model.train()
+        
+        for m in g[1].to_list():
+            inference_result.append(model.inference(m))
+            predict_result.append(model.predict(m))
+        # model refit (change train period and valid period)
+        model.refit()
+
+    inference_result = pd.DataFrame(inference_result, index=test_mons, columns=charas)
+    inference_result.to_csv(f'results/inference/{model.name}_inference.csv')
+    
+    predict_result = pd.DataFrame(predict_result, index=test_mons, columns=charas)
+    predict_result.to_csv(f'results/predict/{model.name}_predict.csv')
+    
+    
+
---
+++ models/PCA.py
@@ -0,0 +1,57 @@
+import sys
+sys.path.append('../')
+
+import pandas as pd
+import numpy as np
+
+from utils import charas
+from .modelBase import modelBase
+
+
+def stock_R_matrix(start_date, end_date):
+    R_matrix = pd.read_pickle('data/stock_R_matrix.pkl')
+    return R_matrix.T.loc[start_date: end_date].T
+
+def portfolio_R_matrix(start_date, end_date):
+    portfolio_ret = pd.read_pickle('data/portfolio_ret.pkl')
+    return portfolio_ret.loc[(portfolio_ret['DATE'] >= start_date) & (portfolio_ret['DATE'] <= end_date)].set_index('DATE').T
+
+
+
+class PCA(modelBase):
+    def __init__(self, K, portfolio=True):
+        super(PCA, self).__init__(f'PCA_{K}')
+        self.K = K
+        self.portfolio = portfolio
+        
+        
+    def __col_de_mean(self, matrix):
+        return (matrix - matrix.mean()).fillna(0)
+    
+        
+    def inference(self, month):
+        if self.portfolio:
+            r_matrix = self.__col_de_mean(portfolio_R_matrix(self.train_period[0], month)).astype(np.float32)
+        else:
+            r_matrix = self.__col_de_mean(stock_R_matrix(self.train_period[0], month))   
+        u, sigma, vt = np.linalg.svd(r_matrix)
+        # B_{N*K}
+        B = u[:, :self.K]
+        # F_{K*1}
+        F = np.diag(sigma[:self.K]) @ vt[:self.K, -1]
+        return B @ F
+        
+            
+    def predict(self, month):
+        if self.portfolio:
+            r_matrix = self.__col_de_mean(portfolio_R_matrix(self.train_period[0], month)).astype(np.float32)
+        else:
+            r_matrix = self.__col_de_mean(stock_R_matrix(self.train_period[0], month))
+        u, sigma, vt = np.linalg.svd(r_matrix)
+        # B_{N*K}
+        B = u[:, :self.K]
+        # F_{K*1}
+        lag_F = np.diag(sigma[:self.K]) @ vt[:self.K, :-1]
+        return B @ np.mean(lag_F, axis=1)
+    
+    
